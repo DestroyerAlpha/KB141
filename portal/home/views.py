bcef49b122b691f4c1001e0e0f81fa65980d1b89
Abhishek Raj <abhiisshheekk@gmail.com>
2020-08-01 18:11:46 +0530

    backend ready for search

--- portal/home/views.py
+++ portal/home/views.py
@@ -22,3 +22,73 @@ def home(request):
         return render(request, 'home/home.html')
     except:
         return render(request, 'home/home.html')
+
+def rs_filter(request):
+    papers = research_paper.objects.all()
+    postpapers = []
+    postpapersbyauth = []
+    postpapersbytag = []
+    users = Profile_student.objects.all()
+    cusers = Profile_corporate.objects.all()
+    papersbyauth = []
+    papersbytag = []
+    userbyname = []
+    userbycollege = []
+    cuserbyname = []
+    cuserbyinsti = []
+    search = False
+    if request.method == 'POST' :
+        name = request.POST.get("search","")
+        if name != "" :
+            search = True
+            #for users(students) filter 
+            userbyname1 = Profile_student.objects.filter(user__username__contains=name)
+            userbyname2 = Profile_student.objects.filter(user__first_name__contains=name)
+            userbyname3 = Profile_student.objects.filter(user__last_name__contains=name)
+            userbyname = (userbyname1 | userbyname2 | userbyname3).distinct()
+            userbycollege = Profile_student.objects.filter(institution__contains = name)
+
+            #for users(company)
+            cuserbyname1 = Profile_corporate.objects.filter(user__username__contains=name)
+            cuserbyname2 = Profile_corporate.objects.filter(user__first_name__contains=name)
+            cuserbyname3 = Profile_corporate.objects.filter(user__last_name__contains=name)
+            cuserbyname = (cuserbyname1 | cuserbyname2 | cuserbyname3).distinct()
+            cuserbyinsti = Profile_corporate.objects.filter(institution__contains = name)
+
+            #for paper filter
+            papersbyauth = research_paper.objects.filter(authors__user__username__contains=name)
+            papersbytag = research_paper.objects.filter(tags__tag__contains=name).distinct()
+
+
+    for p in papers:
+        post = Post.objects.filter(paper__id = p.id)
+        for po in post:
+            postpapers.append(po)
+    
+    for p in papersbyauth:
+        post = Post.objects.filter(paper__id = p.id)
+        for po in post:
+            postpapersbyauth.append(po)
+    
+    for p in papersbytag:
+        post = Post.objects.filter(paper__id = p.id)
+        for po in post:
+            postpapersbytag.append(po)
+
+    resultpapers = zip(postpapers,papers)
+    resultpapersbyauth = zip(postpapersbyauth,papersbyauth)
+    resultpapersbytag = zip(postpapersbytag,papersbytag)
+    rtn = {
+        'name' : name,
+        'search' : search,
+        'papers' : resultpapers,
+        'users' : users,
+        'papersbyauth' : resultpapersbyauth,
+        'papersbytag' : resultpapersbytag,
+        'userbyname' : userbyname,
+        'userbycollege' : userbycollege,
+        'cusers' : cusers,
+        'cuserbyname' : cuserbyname,
+        'cuserbyinsti' : cuserbyinsti,
+    }
+    return render(request, 'home/rs_filter.html', rtn)
