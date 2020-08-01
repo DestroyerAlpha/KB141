42d5ccb21dca12c9c0a2c7dda0fd417ac9af0ae8
Shivam Chaturvedi <shivam4095@gmail.com>
2020-08-01 19:42:54 +0530

    Added feed app

---
+++ portal/feed/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+from ResearchPaper.models import research_paper
+from users.models import Profile_student
+
+# Create your models here.
+
+
