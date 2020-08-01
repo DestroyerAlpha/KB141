bf5fd0e4e1842607782448943eab446981b21920
Abhishek Raj <abhiisshheekk@gmail.com>
2020-08-01 12:48:56 +0530

    added home in url

--- portal/home/urls.py
+++ portal/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    #website-home is set as login redirect in settings.py
+    path('', views.home, name='website-home'),
 ]
