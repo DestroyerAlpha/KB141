a3ca199ece28c3703dcbcea7124ea66a8e7f1c77
Abhishek Raj <abhiisshheekk@gmail.com>
2020-08-01 18:12:54 +0530

    path set for search

--- portal/home/urls.py
+++ portal/home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     #website-home is set as login redirect in settings.py
     path('', views.home, name='website-home'),
+    #path('ufilter/', views.UserFilterView.get_queryset, name='u_filter'),
+    path('rsfilter/', views.rs_filter, name='rsfilter'),
 ]
