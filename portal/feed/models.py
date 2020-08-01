46064cb9d8c6c2bb199b3a7c82a070396f0ea8fe
Shivam Chaturvedi <shivam4095@gmail.com>
2020-08-01 19:45:48 +0530

    Added Post Model

--- portal/feed/models.py
+++ portal/feed/models.py
@@ -7,4 +7,13 @@ from users.models import Profile_student
 
 # Create your models here.
 
+class Post(models.Model):
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(Profile_student, on_delete=models.CASCADE)
+    paper = models.ForeignKey(research_paper,null=True, blank=True, on_delete=models.CASCADE)
+    post_like = models.ManyToManyField(User, blank=True, related_name="post_liked")
+    post_save = models.ManyToManyField(User, blank=True, related_name="post_saved")
+    def get_absolute_url(self):
+        return reverse('feed')
 
